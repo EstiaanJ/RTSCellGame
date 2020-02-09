27fed7177cd41d5fa53501a6f8a6dfae48f24a46
EstiaanJ <xero416@gmail.com>
2020-02-09 16:04:23 +0800

    Started writing movement script

---
+++ RTSCell/Assets/Scripts/MovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementController : MonoBehaviour
+{
+    //using https://www.youtube.com/watch?v=LoKNYlWWeSM as a reference
+    //Note that the video is for MOBA movement not strictly RTS movement.
+
+
+    public LayerMask groundLayer;
+    public NavMeshAgent unitAgent;
+    // Start is called before the first frame update
+    void Awake ()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
